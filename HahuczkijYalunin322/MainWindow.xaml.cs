c142c5c333a0458601a84096e3c3710cf072f2df
227156 <227156@edu.fa.ru>
2025-01-21 10:55:03 +0300

    ПР1

--- HahuczkijYalunin322/MainWindow.xaml.cs
+++ HahuczkijYalunin322/MainWindow.xaml.cs
@@ -15,14 +15,52 @@ using System.Windows.Shapes;
 
 namespace HahuczkijYalunin322
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(X.Text, out double x) && double.TryParse(Y.Text, out double y))
+            {
+                double result = 0;
+                if ((bool)shx.IsChecked)
+                {
+                    result = Math.Sinh(x);
+                }
+                else if ((bool)x2.IsChecked)
+                {
+                    result = Math.Pow(x, 2);
+                }
+                else if ((bool)ex.IsChecked)
+                {
+                    result = Math.Exp(x);
+                }
+                Result.Text = result.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректные значения для x и y.");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            X.Text = string.Empty;
+            Y.Text = string.Empty;
+            Result.Text = string.Empty;
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            var result = MessageBox.Show("Вы уверены, что хотите выйти?", "Подтверждение", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
         }
     }
 }
